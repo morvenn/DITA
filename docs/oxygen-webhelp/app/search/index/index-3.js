dbf4a3ca05add29bb3e0a2bd529ab008abce3e1b
morvenn <morvenn@users.noreply.github.com>
2021-03-14 16:00:31 +0100

    another try

---
+++ docs/oxygen-webhelp/app/search/index/index-3.js
@@ -0,0 +1,4 @@
+define(function () {
+// Auto generated index for searching.
+return {"side":"0*30*-1$-1$-1$9$b$e","się":"2*0*f","step":"3*0*g","style":"0*1*1p$24","tabelce":"2*0*d","table":"2*1*8$b","task":"0*0*2c,3*30*2$9","tej":"2*0*c","third":"1*10*-1$8","title":"0*29*-1$-1$7$q$14","title>user's":"0*10*-1$p","topic":"0*30*1$3,3*0*l","upload":"0*10*-1$u","user":"0*11*-1$r$19","utf":"0*10*-1$o","vertical":"0*0*21","vertical-align":"0*0*20","video":"0*11*-1$10$1i","view":"0*0*2f,3*30*5$c","whatever":"0*0*26","youtube":"0*1*11$1f","znajdują":"2*0*e"};
+});
---
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,11 @@
+define(function() {var keywords=[{w:"First",p:["p0","p1","p2"]},{w:"topic",p:["p0"]},{w:"My",p:["p1","p2"]},{w:"document",p:["p1"]},{w:"reference",p:["p2"]},{w:"Showing",p:["p3"]},{w:"full",p:["p3"]},{w:"task",p:["p3"]},{w:"in",p:["p3"]},{w:"the",p:["p3"]},{w:"Oxygen",p:["p3"]},{w:"author",p:["p3"]},{w:"view",p:["p3"]},{w:"by",p:["p3"]},{w:"default",p:["p3"]}];
+var ph={};
+ph["p0"]=[0, 1];
+ph["p1"]=[2, 0, 3];
+ph["p2"]=[2, 0, 4];
+ph["p3"]=[5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
