972a2bf0e3a226344f478aa0d3c2c7a9e0b8e47a
morvenn <morvenn@users.noreply.github.com>
2021-03-23 17:25:48 +0100

    new documents

--- docs/oxygen-webhelp/app/search/index/keywords.js
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -1,9 +1,17 @@
-define(function() {var keywords=[{w:"First",p:["p0","p1","p2"]},{w:"topic",p:["p0"]},{w:"My",p:["p1","p2"]},{w:"document",p:["p1"]},{w:"reference",p:["p2"]},{w:"Showing",p:["p3"]},{w:"full",p:["p3"]},{w:"task",p:["p3"]},{w:"in",p:["p3"]},{w:"the",p:["p3"]},{w:"Oxygen",p:["p3"]},{w:"author",p:["p3"]},{w:"view",p:["p3"]},{w:"by",p:["p3"]},{w:"default",p:["p3"]}];
+define(function() {var keywords=[{w:"First",p:["p0"]},{w:"topic",p:["p0"]},{w:"Kogo",p:["p1","p8"]},{w:"i",p:["p1"]},{w:"o",p:["p1","p2"]},{w:"co",p:["p1","p2","p6"]},{w:"mog\u0119",p:["p1","p2","p8"]},{w:"pyta\u0107?",p:["p1","p2","p8"]},{w:"do_zrobienia",p:["p3"]},{w:"kolejny",p:["p4"]},{w:"koncepcja",p:["p5"]},{w:"to",p:["p6"]},{w:"jest",p:["p6"]},{w:"informacja",p:["p6"]},{w:"publiczna?",p:["p6"]},{w:"nastepny",p:["p7"]},{w:"Showing",p:["p9"]},{w:"full",p:["p9"]},{w:"task",p:["p9"]},{w:"in",p:["p9"]},{w:"the",p:["p9"]},{w:"Oxygen",p:["p9"]},{w:"author",p:["p9"]},{w:"view",p:["p9"]},{w:"by",p:["p9"]},{w:"default",p:["p9"]},{w:"Tytu\u0142",p:["p10"]},{w:"Jak",p:["p11"]},{w:"zada\u0107",p:["p11"]},{w:"pytanie?",p:["p11"]}];
 var ph={};
 ph["p0"]=[0, 1];
-ph["p1"]=[2, 0, 3];
-ph["p2"]=[2, 0, 4];
-ph["p3"]=[5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+ph["p1"]=[2, 3, 4, 5, 6, 7];
+ph["p11"]=[27, 28, 29];
+ph["p2"]=[4, 5, 6, 7];
+ph["p3"]=[8];
+ph["p4"]=[9];
+ph["p5"]=[10];
+ph["p6"]=[5, 11, 12, 13, 14];
+ph["p7"]=[15];
+ph["p8"]=[2, 6, 7];
+ph["p9"]=[16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+ph["p10"]=[26];
      return {
          keywords: keywords,
          ph: ph
